d23b56f107276b446fa3ddac1df33658b61068bb
BabiychukYuliya <julia4233097@gmail.com>
2023-10-10 17:37:36 +0300

    Create generic.ts

---
+++ generic.ts
@@ -0,0 +1,14 @@
+type MixedType = {
+  [key: string]: string | number;
+};
+
+const userInfo = {
+  name: "Bob",
+  age: 28,
+  country: "United States",
+};
+
+const bookDescription = {
+  title: "Alhimic",
+  pages: 89,
+};

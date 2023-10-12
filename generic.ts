7284f137860eff338849e34a98b8e236f3e81555
BabiychukYuliya <julia4233097@gmail.com>
2023-10-12 08:56:44 +0300

    Update generic.ts

--- generic.ts
+++ generic.ts
@@ -1,5 +1,5 @@
 type MixedType = {
-  [key: string]: string | number;
+  [key: string]: string | number; //індексований тип
 };
 
 const userInfo: MixedType = {
@@ -12,3 +12,23 @@ const bookDescription: MixedType = {
   title: "Alhimic",
   pages: 89,
 };
+
+//створити функцію, яка приймає масив будь-якого типу і повертає масив у зворотньому порядку
+function reverseValues<K>(items: K[]): K[] {
+  return items.reverse();
+}
+
+let numbers = reverseValues<number>([1, 2, 3, 4, 5]); // [5, 4, 3, 2, 1]
+let strings = reverseValues<string>(["a", "b", "c"]); // ["c", "b", "a]
+
+// Створити функцію, яка приймає об'єкт та ключ як рядок.
+// Функція повинна повертати значення цього ключа з об'єкта
+
+const student = {
+  name: "Jon",
+  age: 25,
+};
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
+}
+let studentName = getProperty(student, "name");

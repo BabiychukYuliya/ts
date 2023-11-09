abc790d17123aecad833aeedf9038089e10045c6
BabiychukYuliya <julia4233097@gmail.com>
2023-11-09 15:16:49 +0200

    Update generic2.ts

--- generic2.ts
+++ generic2.ts
@@ -1,7 +1,20 @@
-interface Lengyhwise {
-  length: number;
+// interface Lengyhwise {
+//   length: number;
+// }
+
+// function printLength<T extends Lengyhwise>(arg: T): number {
+//   return arg.length;
+// }
+
+function getProperty<T, K extends keyof T>(obj: T, key: K) {
+  return obj[key];
 }
 
-function printLength<T extends Lengyhwise>(arg: T): number {
-  return arg.length;
-}
+const myObject = {
+  a: 1,
+  b: 2,
+  c: 3,
+};
+
+getProperty(myObject, "b");
+// K === 'a' | 'b' | 'c'
